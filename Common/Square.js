15da6426160cd3e2671b0a261d991f4003d3e951
oukaaron <42984514+oukaaron@users.noreply.github.com>
2018-09-12 21:27:58 -0700

    Update Square.js

--- Common/Square.js
+++ Common/Square.js
@@ -29,11 +29,12 @@ function Square(gl, vertexShaderId, fragmentShaderId) {
 		0.0, 0.0,  // Vertex 0
 		1.0, 0.0,  // Vertex 1
 		1.0, 1.0,  // Vertex 2
-		0.0, 1.0]),       
+		0.0, 1.0   // Vertex 3
+		]),       
 		numComponets : 3 
 	};
 	this.indices = { 
-		values : new Uint16Array([ 0, 1, 3, 2 ]) 
+		values : new Unit16Array([ 0, 1, 3, 2 ]) 
 	};
 	
 	// positions
