d98b39c41bc03ae48c72e63410757a664480f1a7
oukaaron <42984514+oukaaron@users.noreply.github.com>
2018-10-03 14:30:33 -0700

    Update Cube.js

--- Common/Cube.js
+++ Common/Cube.js
@@ -1,4 +1,3 @@
-
 function Cube( vertexShaderId, fragmentShaderId ) {
 
     // Initialize the shader pipeline for this object using either shader ids
@@ -18,60 +17,104 @@ function Cube( vertexShaderId, fragmentShaderId ) {
 
     this.positions = { 
         values : new Float32Array([
-          0.0,0.0,0.0,
-           0.5,0.0,0.0,
-           0.5,0.5,0.0,
-           0.0,0.5,0.0,
             
-           0.5,0.0,0.0,
-           0.5,0.5,0.0,
-           0.5,0.5,0.5,
-           0.5,0.0,0.5,
             
-	   0.5,0.0,0.5,
-           0.0,0.0,0.5,
-           0.0,0.5,0.5,
-           0.5,0.5,0.5,
+           -0.5, -0.5, 0.5,     
+            0.5, -0.5, 0.5,     
+            0.5, 0.5, 0.5,      
+            -0.5, 0.5, 0.5,     
             
-	   0.0,0.0,0.5,
-	   0.0,0.0,0.0,
-	   0.0,0.5,0.0,
-           0.0,0.5,0.5,
-			
-           0.0,0.5,0.0,
-           0.5,0.5,0.0,
-           0.5,0.5,0.5,
-           0.0,0.5,0.5,
+           
+            -0.5, -0.5, -0.5,   
+            0.5, -0.5, -0.5,    
+            0.5, 0.5, -0.5,     
+            -0.5, 0.5, -0.5,    
             
-           0.0,0.0,0.5,
-           0.5,0.0,0.5,
-           0.0,0.0,0.0,
-           0.5,0.0,0.0,
+           
+            0.5, -0.5, 0.5,     
+            0.5, -0.5, -0.5,    
+            0.5, 0.5, -0.5,     
+            0.5, 0.5, 0.5,      
+            
+            
+            -0.5, -0.5, -0.5,   
+            -0.5, -0.5, 0.5,    
+            -0.5, 0.5, 0.5,     
+            -0.5, 0.5, -0.5,    
+            
+           
+            -0.5, 0.5, 0.5,     
+            0.5, 0.5, 0.5,      
+            0.5, 0.5, -0.5,     
+            -0.5, 0.5, -0.5,    
+            
+           
+            -0.5, -0.5, -0.5,   
+            0.5, -0.5, -0.5,    
+            0.5, -0.5, 0.5,     
+            -0.5, -0.5, 0.5     
+            
             ]),
         numComponents : 3
     };
     
     this.indices = { 
         values : new Uint16Array([
-           0,1,3,
-           3,1,2,
+            0, 1, 3,
+            3, 1, 2,
+                  
+            5, 4, 6,
+            6, 4, 7,             
             
-           7,5,4,
-           6,5,7,
+            8, 9, 11,
+            11, 9, 10,
             
-           8,9,11,
-           11,9,10,
+            12, 13, 15,
+            15, 13, 14,
             
-           12,13,15,
-           15,13,14,
+            16, 17, 19,
+            19, 17, 18,
             
-           16,17,19,
-           19,17,18,
-            
-           20,21,22,
-           22,21,23,
+            20, 21, 23,
+            23, 21, 22
         ])
     };
+	
+	this.colors = {
+	values : new Float32Array([
+		1.0, 0.0, 0.0,
+		1.0, 0.0, 0.0,
+		1.0, 0.0, 0.0,
+		1.0, 0.0, 0.0,
+		
+		1.0, 1.0, 0.0,
+		1.0, 1.0, 0.0,
+		1.0, 1.0, 0.0,
+		1.0, 1.0, 0.0,
+		
+		1.0, 1.0, 1.0,
+		1.0, 1.0, 1.0,
+		1.0, 1.0, 1.0,
+		1.0, 1.0, 1.0,
+		
+		0.0, 1.0, 0.0,
+		0.0, 1.0, 0.0,
+		0.0, 1.0, 0.0,
+		0.0, 1.0, 0.0,
+		
+		1.0, 0.0, 1.0,
+		1.0, 0.0, 1.0,
+		1.0, 0.0, 1.0,
+		1.0, 0.0, 1.0,
+		
+		0.0, 1.0, 1.0,
+		0.0, 1.0, 1.0,
+		0.0, 1.0, 1.0,
+		0.0, 1.0, 1.0
+	]),
+	numComponents : 3
+	};
+	
     this.indices.count = this.indices.values.length;
 
     
@@ -86,6 +129,15 @@ function Cube( vertexShaderId, fragmentShaderId ) {
     this.positions.attributeLoc = gl.getAttribLocation( this.program, "vPosition" );
     gl.enableVertexAttribArray( this.positions.attributeLoc );
 
+	//color
+    this.colors.buffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, this.colors.buffer );
+    gl.bufferData( gl.ARRAY_BUFFER, this.colors.values, gl.STATIC_DRAW );
+	
+	this.colors.attributeLoc = gl.getAttribLocation( this.program, "vColors" );
+    gl.enableVertexAttribArray( this.colors.attributeLoc );
+	
+	
     MVLoc = gl.getUniformLocation( this.program, "MV" );
 
     this.MV = undefined;
@@ -95,6 +147,10 @@ function Cube( vertexShaderId, fragmentShaderId ) {
 
         gl.bindBuffer( gl.ARRAY_BUFFER, this.positions.buffer );
         gl.vertexAttribPointer( this.positions.attributeLoc, this.positions.numComponents,
+            gl.FLOAT, gl.FALSE, 0, 0 );
+			
+		gl.bindBuffer( gl.ARRAY_BUFFER, this.colors.buffer );
+        gl.vertexAttribPointer( this.colors.attributeLoc, this.colors.numComponents,
             gl.FLOAT, gl.FALSE, 0, 0 );
  
         gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer );
