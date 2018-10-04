697699b9277a93d260c5a822e0a9c787282c957d
oukaaron <42984514+oukaaron@users.noreply.github.com>
2018-10-03 20:21:02 -0700

    Added Solar.html, Solar.js and SolarSystem.js

---
+++ Assignment-5/SolarSystem.js
@@ -0,0 +1,68 @@
+var SolarSystem = {
+  Sun : {
+    radius : 3,  
+    distance : 0,
+    year : 0,
+    color : [ 1.0, 1.0, 0.0, 1.0 ]
+  },
+  Mercury : {
+    radius : 0.2724,
+    distance : 5,
+    year : 0.241,
+    color : [ 1.0, 0.0, 0.0, 1.0 ]
+  },
+  Venus : {
+    radius : 0.949,
+    distance : 10,
+    year : 0.615,
+    color : [ 1.0, 0.0, 1.0, 1.0 ]
+  },
+  Earth : {
+    radius : 1,
+    distance : 15,
+    year : 1,
+    color : [ 0.0, 0.0, 1.0, 1.0 ]
+  },
+  Moon : {
+    radius : 0.2724,
+    distance : 2.5,
+    year : 0.0748,
+    color : [ 0.5, 0.5, 0.5, 1.0 ]
+  },
+  Mars : {
+    radius : 0.532,
+    distance : 20,
+    year : 1.88,
+    color : [ 1.0, 0.0, 0.0, 1.0 ]
+  },
+  Jupiter : {
+    radius : 5,
+    distance : 30,
+    year : 11.9,
+    color : [ 1.0, 153/255, 0.0, 1.0 ]
+  },
+  Saturn : {
+    radius : 3.5,
+    distance : 45,
+    year : 29.4,
+    color : [ 1.0, 1.0, 0.0, 1.0 ]
+  },
+  Uranus : {
+    radius : 2.5,
+    distance : 55,
+    year : 83.7,
+    color : [ 0.0, 0.1, 1.0, 1.0 ]
+  },
+  Neptune : {
+    radius : 2.5,
+    distance : 65,
+    year : 163.7,
+    color : [ 0.0, 0.0, 1.0, 1.0 ]
+  },
+  Pluto : {
+    radius : 0.3,
+    distance : 75,
+    year : 247.9,
+    color : [ 150/255, 75/255, 0.0, 1.0 ]
+  }
+};
